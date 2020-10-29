1e8c60384e74c1e4dc18a8ef0d09b1703fd17562
Metkel <MetkelK@gmail.com>
2020-10-28 20:45:15 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ class App extends Component {
 	constructor(props) {
     	super(props);
     	this.state = {
-      		sevenDayMostViewedArticles: [],
-			sevenDayArticlesisLoaded: false,
+      	sevenDayMostViewedArticles: [],
+		    sevenDayArticlesisLoaded: false,
 		    topStoriesArticles : [],
 		    topStoriesisLoaded: false,
 		    searchedArticles: [],
