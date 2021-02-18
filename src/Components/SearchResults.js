0406604b3c7cc7bacd3f7bebcc6eca17a6fc9eaa
Metkel <MetkelK@gmail.com>
2021-02-18 18:33:59 -0500

    fixed more formatting

--- src/Components/SearchResults.js
+++ src/Components/SearchResults.js
@@ -19,102 +19,102 @@ import { makeStyles } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 const useStyles = makeStyles(theme => ({
-	title: {
-		margin: 0,
-    	padding: theme.spacing(2),
-	},
-	closeButton: {
-	    position: 'absolute',
-	    right: theme.spacing(1),
-	    top: theme.spacing(1),
-	    color: theme.palette.grey[500],
-  	},
-  	root: {
-	    display: 'flex',
-	    flexWrap: 'wrap',
-	    justifyContent: 'space-around',
-	    overflow: 'hidden',
-	},
-	gridList: {
-    	width: '100%',
-    	height: 'auto',
-    	transform: 'translateZ(0)',
-  	},
-  	icon: {
-    	color: '#FFF',
-  	},
+    title: {
+        margin: 0,
+        padding: theme.spacing(2),
+    },
+    closeButton: {
+        position: 'absolute',
+        right: theme.spacing(1),
+        top: theme.spacing(1),
+        color: theme.palette.grey[500],
+    },
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        overflow: 'hidden',
+    },
+    gridList: {
+        width: '100%',
+        height: 'auto',
+        transform: 'translateZ(0)',
+    },
+    icon: {
+        color: '#FFF',
+    },
 }));
 
 function SearchResults(props){
-	const { searchresults, searchedArticlesLoaded, searchCompleted } = props;
-	
-	const [open, setOpen] = useState(false);
+    const { searchresults, searchedArticlesLoaded, searchCompleted } = props;
+    
+    const [open, setOpen] = useState(false);
 
-	const handleClose = () => {
-	    setOpen(false);
-	    searchCompleted();
-	};
+    const handleClose = () => {
+        setOpen(false);
+        searchCompleted();
+    };
 
-	useEffect(() => {
-		if (searchedArticlesLoaded) {
-			setOpen(true);
-		} else {
-			setOpen(false);
-		}
-	}, [searchedArticlesLoaded])
+    useEffect(() => {
+        if (searchedArticlesLoaded) {
+            setOpen(true);
+        } else {
+            setOpen(false);
+        }
+    }, [searchedArticlesLoaded])
 
-	const classes = useStyles();
+    const classes = useStyles();
 
-	const matches = useMediaQuery('(min-width:960px)');
+    const matches = useMediaQuery('(min-width:960px)');
 
-	if (searchedArticlesLoaded) {
-		return (
-			<div>	
-		      	<Dialog
-			        open={open}
-			        onClose={handleClose}
-			        aria-labelledby="alert-dialog-title"
-			        aria-describedby="alert-dialog-description"
-			        fullWidth
-			        maxWidth='lg'
-			        fullScreen={matches ? false : true}
-		      	>
-			        <DialogTitle id="alert-dialog-title" disableTypography className={classes.title}>
-			        	<Typography variant="h6">Search Results</Typography>
-			        	<IconButton aria-label="close" className={classes.closeButton} onClick={handleClose}>
-			          		<CloseIcon />
-			        	</IconButton>
-			        </DialogTitle>
-			        <DialogContent>
-			        	<div className={classes.root}>
-							<GridList cellHeight='auto' className={classes.gridList} >
-								{searchresults.map((result, i) => (
-									<GridListTile key={result._id} cols={matches ? 1 : 2}>
-										{(result.multimedia.length > 0) ? <img 
-											src={"http://www.nytimes.com/" + result.multimedia[0].url}
-											alt='alt'
-										/> : null }
-										<GridListTileBar
-											title={result.headline.main}
-							              	subtitle={<span>{result.byline.original}</span>}
-							              	actionIcon={
-								            <IconButton aria-label={`info about ${result.title}`} className={classes.icon}>
-								                <a href={result.web_url} target="_blank" rel="noopener noreferrer"><InfoIcon /></a>
-								            </IconButton>
-							            }
-							            />
-							        </GridListTile>
-								))}
-							</GridList>
-						</div>
-			        </DialogContent>
-      			</Dialog>
-			</div>	
-		);
-	} else {
-		return null;
-	}
-	
+    if (searchedArticlesLoaded) {
+        return (
+            <div>   
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                    fullWidth
+                    maxWidth='lg'
+                    fullScreen={matches ? false : true}
+                >
+                    <DialogTitle id="alert-dialog-title" disableTypography className={classes.title}>
+                        <Typography variant="h6">Search Results</Typography>
+                        <IconButton aria-label="close" className={classes.closeButton} onClick={handleClose}>
+                            <CloseIcon />
+                        </IconButton>
+                    </DialogTitle>
+                    <DialogContent>
+                        <div className={classes.root}>
+                            <GridList cellHeight='auto' className={classes.gridList} >
+                                {searchresults.map((result, i) => (
+                                    <GridListTile key={result._id} cols={matches ? 1 : 2}>
+                                        {(result.multimedia.length > 0) ? <img 
+                                            src={"http://www.nytimes.com/" + result.multimedia[0].url}
+                                            alt='alt'
+                                        /> : null }
+                                        <GridListTileBar
+                                            title={result.headline.main}
+                                            subtitle={<span>{result.byline.original}</span>}
+                                            actionIcon={
+                                            <IconButton aria-label={`info about ${result.title}`} className={classes.icon}>
+                                                <a href={result.web_url} target="_blank" rel="noopener noreferrer"><InfoIcon /></a>
+                                            </IconButton>
+                                        }
+                                        />
+                                    </GridListTile>
+                                ))}
+                            </GridList>
+                        </div>
+                    </DialogContent>
+                </Dialog>
+            </div>  
+        );
+    } else {
+        return null;
+    }
+    
 }
 
 export default SearchResults;
--- src/Components/TopStories.js
+++ src/Components/TopStories.js
@@ -15,78 +15,78 @@ import Carousel from 'react-material-ui-carousel'
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
-	root: {
-		alignItems: 'center',
-	  	display: 'flex',
-	  	height: '100%',
-	  	justifyContent: 'center',
-	  	overflowX: 'hidden',
-	  	width: '90vmin',
-	},
-	img: {
-	    maxHeight: '40vh',
-	    marginBottom: '0.5rem',
-	},
-  	button: {
-  		display: 'flex',
-    	justifyContent: 'center',
-    	width: '100%',
-  	}
+    root: {
+        alignItems: 'center',
+        display: 'flex',
+        height: '100%',
+        justifyContent: 'center',
+        overflowX: 'hidden',
+        width: '90vmin',
+    },
+    img: {
+        maxHeight: '40vh',
+        marginBottom: '0.5rem',
+    },
+    button: {
+        display: 'flex',
+        justifyContent: 'center',
+        width: '100%',
+    }
 });
 
 function Item(props) {
-	const classes = useStyles();
+    const classes = useStyles();
 
-	return (
-		<Card>
-			<CardActionArea>
-				<CardContent>
-					<CardMedia
-						classes={{ img: classes.img}}
-				        image={props.result.multimedia[0].url}
-				        title={props.result.multimedia[0].caption}
-				        component="img"
-				    />
-				    <Typography gutterBottom variant='h6' align='center'>
+    return (
+        <Card>
+            <CardActionArea>
+                <CardContent>
+                    <CardMedia
+                        classes={{ img: classes.img}}
+                        image={props.result.multimedia[0].url}
+                        title={props.result.multimedia[0].caption}
+                        component="img"
+                    />
+                    <Typography gutterBottom variant='h6' align='center'>
                       {props.result.title}
-                  	</Typography>
-                   	<Typography variant='body2' color='textSecondary'>
-                    	{props.result.abstract}
                     </Typography>
-				</CardContent>
-			</CardActionArea>
-			<CardActions>
-				<Button className={classes.button} size='medium' > 
-                	<a href={props.result.url} target="_blank" rel="noopener noreferrer">Read More</a>
-              	</Button>
-			</CardActions>
-		</Card>
-	)
+                    <Typography variant='body2' color='textSecondary'>
+                        {props.result.abstract}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button className={classes.button} size='medium' > 
+                    <a href={props.result.url} target="_blank" rel="noopener noreferrer">Read More</a>
+                </Button>
+            </CardActions>
+        </Card>
+    )
 }
 
 function TopStories(props) {
-	const { topstories, topstoriesLoaded } = props;
+    const { topstories, topstoriesLoaded } = props;
 
-	const classes = useStyles();
+    const classes = useStyles();
 
-	if (topstoriesLoaded) {
-		return (
-			<Container className={classes.root}>
-				<Carousel 
-					autoPlay={true}
-					animation='slide'
-					indicators={true}
-					navButtonsAlwaysVisible={false}
-					>
-					{topstories.map((result, i) => (
-						<Item result={result} key={i} />
-					))}
-				</Carousel>
-			</Container>
-		);
-	} else {
-		  return null;
-	}
+    if (topstoriesLoaded) {
+        return (
+            <Container className={classes.root}>
+                <Carousel 
+                    autoPlay={true}
+                    animation='slide'
+                    indicators={true}
+                    navButtonsAlwaysVisible={false}
+                    >
+                    {topstories.map((result, i) => (
+                        <Item result={result} key={i} />
+                    ))}
+                </Carousel>
+            </Container>
+        );
+    } else {
+        return null;
+    }
 }
 
 export default TopStories;
