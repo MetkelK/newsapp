210dbbb83013dadafda88c3c2556130c005a39ac
Metkel <MetkelK@gmail.com>
2021-02-18 18:30:41 -0500

    fixed formatting

--- src/App.js
+++ src/App.js
@@ -15,78 +15,74 @@ const NYT_SEARCH_URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json
 
 class App extends Component {
 	constructor(props) {
-    	super(props);
-    	this.state = {
-      	sevenDayMostViewedArticles: [],
-		    sevenDayArticlesisLoaded: false,
-		    topStoriesArticles : [],
-		    topStoriesisLoaded: false,
-		    searchedArticles: [],
-		    searchedArticlesLoaded: false,
-		    query: ''
-    	};
-  	}
+    super(props);
+    this.state = {
+      sevenDayMostViewedArticles: [],
+		  sevenDayArticlesisLoaded: false,
+		  topStoriesArticles : [],
+		  topStoriesisLoaded: false,
+		  searchedArticles: [],
+		  searchedArticlesLoaded: false,
+		  query: ''
+    };
+  }
 
 	componentDidMount(){
 		this.getArticles();
 	}
 
 	getArticles(){
-    	fetch(`${NYT_SEVENDAYVIEW_URL}api-key=${process.env.REACT_APP_API_KEY}`)
-    		.then((res) => res.json())
-      		.then((res) => {
-        		this.setState({ sevenDayMostViewedArticles: res.results, sevenDayArticlesisLoaded: true})
-      		})
-    		.catch(err => console.log(err));
-    	fetch(`${NYT_TOPSTORIES_URL}api-key=${process.env.REACT_APP_API_KEY}`)
-    		.then((res) => res.json())
-    		.then((res) => {
-        		this.setState({ topStoriesArticles:res.results, topStoriesisLoaded: true })
-      		})
-      		.catch(err => console.log(err));
-  	}
+        fetch(`${NYT_SEVENDAYVIEW_URL}api-key=${process.env.REACT_APP_API_KEY}`)
+    	.then((res) => res.json())
+        .then((res) => {
+            this.setState({ sevenDayMostViewedArticles: res.results, sevenDayArticlesisLoaded: true})
+        })
+    	.catch(err => console.log(err));
+        fetch(`${NYT_TOPSTORIES_URL}api-key=${process.env.REACT_APP_API_KEY}`)
+    	.then((res) => res.json())
+    	.then((res) => {
+            this.setState({ topStoriesArticles:res.results, topStoriesisLoaded: true })
+        })
+        .catch(err => console.log(err));
+    }
 
-  	handleSearch = (e) => {
-	    this.setState({
-	    	query: e.target.value
-	    });
-  	};
+    handleSearch = (e) => {
+	   this.setState({
+	       query: e.target.value
+	   });
+    }
 
-  	searchArticles = (e) => {
-    	e.preventDefault();
-    	fetch(`${NYT_SEARCH_URL}${this.state.query}&api-key=${process.env.REACT_APP_API_KEY}`)
-      		.then((res) => res.json())
-      		.then((res) => {
-        		this.setState({ searchedArticles: res.response.docs, searchedArticlesLoaded: true})
-      		})
-      		.catch(err => console.log(err));
-  	}
+    searchArticles = (e) => {
+        e.preventDefault();
+        fetch(`${NYT_SEARCH_URL}${this.state.query}&api-key=${process.env.REACT_APP_API_KEY}`)
+        .then((res) => res.json())
+        .then((res) => {
+            this.setState({ searchedArticles: res.response.docs, searchedArticlesLoaded: true})
+        })
+        .catch(err => console.log(err));
+    }
 
-  	searchCompleted = () => {
-    	this.setState({ searchedArticles: [], searchedArticlesLoaded: false });
-  	}
+    searchCompleted = () => {
+        this.setState({ searchedArticles: [], searchedArticlesLoaded: false });
+    }
 
-  	render() {
-    	return (
-      		<React.Fragment>
-        		<Navbar handleSearch={this.handleSearch} searchArticles={this.searchArticles}></Navbar>
-
-		        <div className="headlines" >
-		          <TopStories topstories={this.state.topStoriesArticles} topstoriesLoaded={this.state.topStoriesisLoaded} />
-		        </div>
-
-		        <Container>
-		          <MostViewed mostviewed={this.state.sevenDayMostViewedArticles} mostviewedLoaded={this.state.sevenDayArticlesisLoaded}/>
-		        </Container>
-
-		        <Container>
-		          <SearchResults searchresults={this.state.searchedArticles} searchedArticlesLoaded={this.state.searchedArticlesLoaded} searchCompleted={this.searchCompleted}/>
-		        </Container>
-
-        		<p style={{ textAlign: 'center', margin: '1rem 0' }}>Data provided by <a href="https://www.nytimes.com" target="_blank" rel="noopener noreferrer">The New York Times</a></p>
-      		</React.Fragment>
-    	);
-  	}
+    render() {
+        return (
+            <React.Fragment>
+                <Navbar handleSearch={this.handleSearch} searchArticles={this.searchArticles}></Navbar>
+    		    <div className="headlines" >
+    		      <TopStories topstories={this.state.topStoriesArticles} topstoriesLoaded={this.state.topStoriesisLoaded} />
+    		    </div>
+    		    <Container>
+    		      <MostViewed mostviewed={this.state.sevenDayMostViewedArticles} mostviewedLoaded={this.state.sevenDayArticlesisLoaded}/>
+    		    </Container>
+    		    <Container>
+    		      <SearchResults searchresults={this.state.searchedArticles} searchedArticlesLoaded={this.state.searchedArticlesLoaded} searchCompleted={this.searchCompleted}/>
+    		    </Container>
+                <p style={{ textAlign: 'center', margin: '1rem 0' }}>Data provided by <a href="https://www.nytimes.com" target="_blank" rel="noopener noreferrer">The New York Times</a></p>
+            </React.Fragment>
+        );
+    }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { render, unmountComponentAtNode  } from 'react-dom';
+import { act } from "react-dom/test-utils";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from './App';
+import TopStories from './Components/TopStories';
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
 });
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it('renders a footer with info', () => {
+  act(() => {
+  	render(<App />, container);
+  });
+  expect(container.textContent).toMatch('Data provided by');
+});
+
